fa48cc40f08ce186eb6119be60170576e119ac11
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-10-17 18:56:41 +0300

    first master commit

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <cmath>
+#include "../PR 5.1/PR 5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+    TEST_CLASS(UnitTest2)
+    {
+    public:
+
+        TEST_METHOD(TestKFunction)
+        {
+           
+            double x = 1.0;
+            double y = 2.0;
+            double expectedValue = (sin(x) / (y * y)) + (cos(y) / (x * x));
+
+            
+            double result = k(x, y);
+
+            
+            Assert::AreEqual(result, expectedValue, 1e-6); 
+        }
+    };
+}
+
